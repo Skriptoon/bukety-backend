6d008bfa3261a92b41294128db29e917c9774e06
Pavel Deev <pavel.deev45@gmail.com>
2025-05-31 02:02:09 +0500

    phpdoc

--- app/Filters/Product/Ids.php
+++ app/Filters/Product/Ids.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Filters\Product;
 
+use App\Models\Product\Product;
 use Illuminate\Database\Eloquent\Builder;
 use Lacodix\LaravelModelFilter\Filters\Filter;
 
 class Ids extends Filter
 {
     /**
+     * @param Builder<Product> $query
+     * @return Builder<Product>
      */
     public function apply(Builder $query): Builder
     {
